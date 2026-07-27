35e30673ff95b0fd980acdf71570bd273870d370
chandan pandit <chandanpandit@2102>
2026-07-27 11:46:26 +0530

    commit

---
+++ codes/argumentpostuion.py
@@ -0,0 +1,6 @@
+def sum (a,b):
+    print(f"the sum of your number is {a + b}")
+
+
+sum (12)
+sum (16,18)    
---
+++ codes/defaultargument.py
@@ -0,0 +1,4 @@
+def hello(name,age):
+    print(f"your name is {name}and your age is {age}")
+hello(age =25,name="chandan")  
+
